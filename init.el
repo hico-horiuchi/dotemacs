c45dee70ae4124e0f216d9097b11a6713541289e
Akihiko Horiuchi <12ff5b8@gmail.com>
2014-09-07 20:30:36 +0900

    themeをディレクトリに分ける

--- init.el
+++ init.el
@@ -53,7 +53,7 @@
 ;;--------------------------------------------------------------------------------
 (custom-set-variables
  '(custom-safe-themes (quote ("f07583bdbcca020adecb151868c33820dfe3ad5076ca96f6d51b1da3f0db7105" default))))
-(add-to-list 'custom-theme-load-path (concat user-emacs-directory "elisp/replace-colorthemes"))
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "theme/replace-colorthemes"))
 (load-theme 'dark-laptop)
 (custom-set-faces
  '(default ((t (:background "unspecified" :foreground "unspecified")))))
