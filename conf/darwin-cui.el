dde065b58d76f2fc77317495eec5149936bc0d50
Akihiko Horiuchi <12ff5b8@gmail.com>
2016-11-02 00:37:34 +0900

    :+1: OS X用のCopy/Pasteの設定を追加

--- conf/darwin-cui.el
+++ conf/darwin-cui.el
@@ -1,6 +1,20 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
 ;;--------------------------------------------------------------------------------
+;; Clipboard
+;;--------------------------------------------------------------------------------
+(setq interprogram-cut-function
+      (lambda (text &optional push)
+        (let ((process-connection-type nil))
+          (let ((proc (start-process "pbcopy" nil "pbcopy")))
+            (process-send-string proc text)
+            (process-send-eof proc)))))
+(setq interprogram-paste-function
+      (lambda ()
+        (shell-command-to-string "pbpaste")))
+;;--------------------------------------------------------------------------------
+
+;;--------------------------------------------------------------------------------
 ;; Dictionary.app
 ;;--------------------------------------------------------------------------------
 (global-set-key (kbd "C-c w o")
